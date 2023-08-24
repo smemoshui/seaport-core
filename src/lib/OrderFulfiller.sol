20c50a09b5058f392a7afe6f0a498e7d9ce8c792
moshui <sme_moshui@outlook.com>
2023-08-24 13:48:49 +0800

    fix

--- src/lib/OrderFulfiller.sol
+++ src/lib/OrderFulfiller.sol
@@ -48,4 +48,41 @@ contract OrderFulfiller is BasicOrderFulfiller, CriteriaResolution, AmountDerive
      *                          ERC20/721/1155 tokens.
      */
     constructor(address conduitController) BasicOrderFulfiller(conduitController) {}
+
+    /**
+     * @dev Internal function to emit an OrderFulfilled event. OfferItems are
+     *      translated into SpentItems and ConsiderationItems are translated
+     *      into ReceivedItems.
+     *
+     * @param orderHash     The order hash.
+     * @param offerer       The offerer for the order.
+     * @param zone          The zone for the order.
+     * @param recipient     The recipient of the order, or the null address if
+     *                      the order was fulfilled via order matching.
+     * @param offer         The offer items for the order.
+     * @param consideration The consideration items for the order.
+     */
+    function _emitOrderFulfilledEvent(
+        bytes32 orderHash,
+        address offerer,
+        address zone,
+        address recipient,
+        OfferItem[] memory offer,
+        ConsiderationItem[] memory consideration
+    ) internal {
+        // Cast already-modified offer memory region as spent items.
+        SpentItem[] memory spentItems;
+        assembly {
+            spentItems := offer
+        }
+
+        // Cast already-modified consideration memory region as received items.
+        ReceivedItem[] memory receivedItems;
+        assembly {
+            receivedItems := consideration
+        }
+
+        // Emit an event signifying that the order has been fulfilled.
+        emit OrderFulfilled(orderHash, offerer, zone, recipient, spentItems, receivedItems);
+    }
 }
